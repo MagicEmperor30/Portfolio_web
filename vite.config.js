cb6aa80da6bc719a060ab119281d13226426bf1b
Digvijay <piyushsasane30@gmail.com>
2025-07-03 15:38:54 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/digvijay-portfolio-web/",
+  // base:"/digvijay-portfolio-web/",
   build: {
     outDir: 'dist', // Ensures build output goes into the 'dist' folder
   },
